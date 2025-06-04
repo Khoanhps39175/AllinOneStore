f70a657bd6f51609b95f644a4c173e8fd426b04a
Khoanhps39175 <Khoanhps39175@gmail.com>
2025-06-05 02:09:25 +0700

    Code front end sản phẩm chi tiết

--- AOS_FE/AOS_FE/src/App.vue
+++ AOS_FE/AOS_FE/src/App.vue
@@ -2,6 +2,7 @@
 import ImageUpload from './components/Module/ImageUpload.vue';
 import Login from './components/Module/Login.vue';
 import Register from './components/Module/Register.vue';
+import ProductDetails from './components/User/ProductDetails.vue';
 </script>
 
 <template>
